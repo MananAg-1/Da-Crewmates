8156f7d9e3f1a9b93a9b83784501ff7e44a49704
Divyam201218 <166046989+Divyam201218@users.noreply.github.com>
2026-07-10 10:56:47 +0530

    Update TutorialModal.jsx

--- Da-Crewmates-main/src/components/TutorialModal.jsx
+++ Da-Crewmates-main/src/components/TutorialModal.jsx
@@ -42,7 +42,7 @@ export default function TutorialModal({ user, onClose }) {
           flexDirection: "column",
           gap: 12,
           overflow: "hidden",
-          color: "#000",
+          color: "#fff",
         }}
       >
         {/* Header */}
